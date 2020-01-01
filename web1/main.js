7c3390eef991566f297cac89318f03095543bafb
unknown <ililil9482@naver.com>
2020-01-01 11:28:16 +0900

    main page 구현

--- web1/main.js
+++ web1/main.js
@@ -9,12 +9,11 @@ var app = http.createServer(function(request,response){
     var pathname = url.parse(_url, true).pathname;
 
     if(pathname === '/'){
-      //url 정보를 분석하여 console에 나타내주는 코드
-      //console.log(url.parse(_url, true));
 
-      //파일을 읽어서 description에 내용으로 변환하여 웹페이지를 동작하게 만듦
-      //서버를 껐다 키지 않아도 페이지가 실행될 때 마다 자동으로 파일을 읽어오기 때문에 바로 바로 적용됨
-      fs.readFile(`../data/${queryData.id}`, 'utf-8', function(err, description){
+      if(queryData.id === undefined){
+          fs.readFile(`../data/${queryData.id}`, 'utf-8', function(err, description){
+            var title = `Welcome`;
+            var description = 'Hello, Node.js';
             var template = `
             <!doctype html>
             <html>
@@ -34,14 +33,47 @@ var app = http.createServer(function(request,response){
             </body>
             </html>
             `;
-
-            //200신호는 성공적으로 전송했다.
-            response.writeHead(200);
+              //200신호는 성공적으로 전송했다.
+          response.writeHead(200);
             response.end(template);
+        });
+      } else{
 
-      })
-      //console.log(__dirname + url);
-      //사용자에게 값을 반환해주는 곳
+              //url 정보를 분석하여 console에 나타내주는 코드
+              //console.log(url.parse(_url, true));
+
+              //파일을 읽어서 description에 내용으로 변환하여 웹페이지를 동작하게 만듦
+              //서버를 껐다 키지 않아도 페이지가 실행될 때 마다 자동으로 파일을 읽어오기 때문에 바로 바로 적용됨
+              fs.readFile(`../data/${queryData.id}`, 'utf-8', function(err, description){
+                    var template = `
+                    <!doctype html>
+                    <html>
+                    <head>
+                      <title>WEB1 - ${title}</title>
+                      <meta charset="utf-8">
+                    </head>
+                    <body>
+                      <h1><a href="/">WEB</a></h1>
+                      <ul>
+                        <li><a href="/?id=HTML">HTML</a></li>
+                        <li><a href="/?id=CSS">CSS</a></li>
+                        <li><a href="/?id=JavaScript">JavaScript</a></li>
+                      </ul>
+                      <h2>${queryData.id}</h2>
+                      <p>${description}</p>
+                    </body>
+                    </html>
+                    `;
+
+                    //200신호는 성공적으로 전송했다.
+                    response.writeHead(200);
+                    response.end(template);
+
+              });
+              //console.log(__dirname + url);
+              //사용자에게 값을 반환해주는 곳
+      }
+
     } else{
       //404신호는 파일을 찾을 수 없을 때
       response.writeHead(404);
